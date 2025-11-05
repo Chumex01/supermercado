e55f7af1cbd3fd85a4d84e55a69cebde2978c2cb
Chumex01 <Chuma@gmail.com>
2025-11-05 02:12:16 -0400

    ya esta en la laptop

--- frontend/.next/dev/static/development/_buildManifest.js
+++ frontend/.next/dev/static/development/_buildManifest.js
@@ -2,17 +2,11 @@ self.__BUILD_MANIFEST = {
   "/": [
     "static/chunks/pages/index.js"
   ],
-  "/_error": [
-    "static/chunks/pages/_error.js"
-  ],
   "/empleados": [
     "static/chunks/pages/empleados.js"
   ],
   "/home": [
     "static/chunks/pages/home.js"
-  ],
-  "/login": [
-    "static/chunks/pages/login.js"
   ],
   "/sucursales": [
     "static/chunks/pages/sucursales.js"
